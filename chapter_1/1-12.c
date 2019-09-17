cfdbfc0753a6f4efe8f6a52fd0f64fa9ea21e79e
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2019-09-17 17:07:22 +1000

    Add exercise

---
+++ chapter_1/1-12.c
@@ -0,0 +1,32 @@
+/**
+ * Write a program that prints its input one word per line.
+ */
+
+#include <stdio.h>
+
+#define FALSE 0;
+#define TRUE 1;
+
+int main()
+{
+    int previous_char_is_white_space = TRUE;
+
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        int current_char_is_white_space = c == ' ' || c == '\n' || c == '\t';
+        if (current_char_is_white_space)
+        {
+            if (!previous_char_is_white_space)
+            {
+                putchar('\n');
+            }
+        }
+        else
+        {
+            putchar(c);
+        }
+
+        previous_char_is_white_space = current_char_is_white_space;
+    }
+}
