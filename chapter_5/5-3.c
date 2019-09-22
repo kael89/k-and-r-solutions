11b10bbeac636488283f72ff162b55fdc562bb4c
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2019-09-22 12:42:14 +1000

    Add 5-3.,5-4

---
+++ chapter_5/5-3.c
@@ -0,0 +1,67 @@
+/**
+ * Write a pointer version of the function strcat that we showed in Chapter 2:
+ * strcat(s,t) copies the string t to the end of s.
+ */
+
+#include <stdio.h>
+
+#define bool int
+#define MAX_LENGTH 100
+
+void read_str(char *str);
+char *strcat(char *str1, char *str2);
+
+int main()
+{
+    char str1[MAX_LENGTH];
+    char str2[MAX_LENGTH];
+
+    read_str(str1);
+    read_str(str2);
+    strcat(str1, str2);
+    printf("%s\n", str1);
+
+    return 0;
+}
+
+void read_str(char *str)
+{
+    while ((*str = getchar()) != '\n')
+    {
+        str++;
+        ;
+    }
+    *str = '\0';
+}
+
+char *strcat2(char *str1, char *str2)
+{
+    while (*str1)
+    {
+        str1++;
+    }
+
+    while (*str2)
+    {
+        *str1 = *str2;
+        str1++;
+        str2++;
+    }
+    *str1 = '\0';
+
+    return str1;
+}
+
+char *strcat(char *str1, char *str2)
+{
+    while (*str1)
+    {
+        str1++;
+    }
+    while (*str1++ = *str2++)
+    {
+        ;
+    }
+
+    return str1;
+}
---
+++ chapter_5/5-4.c
@@ -0,0 +1,83 @@
+/**
+ * Write the function strend(s,t), which returns 1 if the string t occurs
+ * at the end of the string s, and zero otherwise.
+ */
+
+#include <stdio.h>
+
+#define MAX_LENGTH 100
+
+void read_str(char *str);
+int str_len(char *s);
+int strend(char *s, char *t);
+
+int main()
+{
+    char str1[MAX_LENGTH];
+    char str2[MAX_LENGTH];
+
+    read_str(str1);
+    read_str(str2);
+    int result = strend(str1, str2);
+    printf("%d\n", result);
+
+    return 0;
+}
+
+void read_str(char *str)
+{
+    while ((*str = getchar()) != '\n')
+    {
+        str++;
+        ;
+    }
+    *str = '\0';
+}
+
+int strend(char *s, char *t)
+{
+    int length_s = str_len(s);
+    int length_t = str_len(t);
+
+    if (length_t > length_s)
+    {
+        return 0;
+    }
+
+    s += length_s;
+    t += length_t;
+    while (length_t-- > 0 && *--s == *--t)
+    {
+    }
+
+    return *s == *t;
+}
+
+int strend1(char *s, char *t)
+{
+    int length_s = str_len(s);
+    int length_t = str_len(t);
+
+    if (length_t > length_s)
+    {
+        return 0;
+    }
+
+    while (length_t-- > 0 && *s-- == *t--)
+    {
+        ;
+    }
+
+    return *s == *t;
+}
+
+int str_len(char *s)
+{
+    int i = 0;
+    while (s[i])
+    {
+        i++;
+    }
+
+    return i;
+}
