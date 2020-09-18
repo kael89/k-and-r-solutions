2236dde941c6854651f035b8b30782eeb90a9ac8
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-09-19 09:29:44 +1000

    Add 1-6

---
+++ chapter_1/1-6.c
@@ -0,0 +1,16 @@
+/**
+ * @description Verify that the expression getchar() != EOF is 0 or 1. 
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int c;
+    do
+    {
+        c = getchar() != EOF;
+        putchar(c + '0');
+    } while (c);
+    printf("\n");
+}
