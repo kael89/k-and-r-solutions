65171acde15d3f330d55031de146e35d6b3a6154
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-10-17 21:28:46 +1100

    Use numeric ordering for exercises

--- run.sh
+++ run.sh
@@ -3,26 +3,38 @@
 DIR=$(dirname "$0")
 . ${DIR}/utils.sh
 
+function get_exer_path() {
+    chapter=$1
+    exer=$2
+    echo "chapter_$chapter/$chapter-$exer.c"
+}
+
+function print_exer_name() {
+    chapter=$1
+    exer=$2
+    log_success "* $chapter-$exer.c"
+}
+
 function run_all() {
     log_success "Running all exercises..."
 
-    local chapter_folders=$(ls | grep chapter_)
+    local chapters=$(ls | grep chapter_ | sed -e 's/chapter_\(.*\)/\1/' | sort -n)
     local passed=0
     local skipped=0
     local failed=0
 
-    for folder in ${chapter_folders[@]}; do
-        local ex_files=$(ls $folder | grep .c)
-        for ex_file in ${ex_files[@]}; do
-            print_filename $ex_file
+    for chapter in ${chapters[@]}; do
+        local exers=$(ls "chapter_$chapter" | sed -e 's/.*-\(.*\).c/\1/' | sort -n)
+        for exer in ${exers[@]}; do
+            print_exer_name $chapter $exer
 
-            local exec_path="$folder/$ex_file"
-            cat $exec_path | grep @interactive >/dev/null 2>&1
+            local exer_path=$(get_exer_path $chapter $exer)
+            cat $exer_path | grep @interactive >/dev/null 2>&1
             if [[ $? = 0 ]]; then
                 log_info "  Interactive program, skipping..."
                 skipped=$((skipped + 1))
             else
-                run_one $exec_path
+                run_one $exer_path
                 if [[ $? > 0 ]]; then
                     failed=$((failed + 1))
                 else
@@ -67,19 +79,10 @@ function run_one() {
     return $result
 }
 
-function print_filename() {
-    log_success "* $1"
-}
-
 if [ "$#" = 0 ]; then
     run_all
 fi
 
-chapter=$1
-exercise=$2
-folder="chapter_$chapter"
-ex_file="$chapter-$exercise.c"
-
-print_filename $ex_file
-run_one "$folder/$ex_file"
+print_exer_name $1 $2
+run_one $(get_exer_path $1 $2)
 exit $?
