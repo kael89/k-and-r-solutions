4f92daf55649d883550f262aba5cfb5f995ed479
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2021-03-17 20:06:04 +1100

    Add test_equal_doubles

--- lib/test.c
+++ lib/test.c
@@ -1,3 +1,5 @@
+#include <float.h>
+#include <math.h>
 #include <stdio.h>
 #include "output.h"
 #include "string.h"
@@ -5,6 +7,7 @@
 typedef enum
 {
     CHAR,
+    DOUBLE,
     INT,
     STRING,
 } type;
@@ -20,6 +23,11 @@ bool compare_typed_values(type t, void *a_p, void *b_p)
     {
     case CHAR:
         return *((char *)a_p) == *((char *)b_p);
+    case DOUBLE:;
+        double a = *((double *)a_p);
+        double b = *((double *)b_p);
+        double maximum = fmax(fabs(a), fabs(b));
+        return fabs(a - b) <= FLT_EPSILON * maximum;
     case INT:
         return *((int *)a_p) == *((int *)b_p);
     case STRING:
@@ -36,6 +44,9 @@ void print_typed_value(type t, void *value_p)
     {
     case CHAR:
         printf("%c", *((char *)value_p));
+        break;
+    case DOUBLE:
+        printf("%f", *((double *)value_p));
         break;
     case INT:
         printf("%d", *((int *)value_p));
@@ -72,14 +83,19 @@ bool test_equal(type t, char description[], void *received_p, void *expected_p)
     }
 }
 
-bool test_equal_ints(char description[], int received, int expected)
-{
-    return test_equal(INT, description, &received, &expected);
-}
-
 bool test_equal_chars(char description[], char received, char expected)
 {
     return test_equal(CHAR, description, &received, &expected);
+}
+
+bool test_equal_doubles(char description[], double received, double expected)
+{
+    return test_equal(DOUBLE, description, &received, &expected);
+}
+
+bool test_equal_ints(char description[], int received, int expected)
+{
+    return test_equal(INT, description, &received, &expected);
 }
 
 bool test_equal_strings(char description[], char received[], char expected[])
--- lib/test.h
+++ lib/test.h
@@ -1,3 +1,4 @@
+bool test_equal_doubles(char description[], double received, double expected);
 bool test_equal_ints(char description[], int received, int expected);
 bool test_equal_chars(char description[], char results, char expected);
 bool test_equal_strings(char description[], char received[], char expected[]);
